2fb85b80761ff47f9e58808b053d2a774d850732
Sbaayel <baayel@gmail.com>
2020-10-01 18:07:37 -0400

    incomplete form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -28,13 +28,13 @@ class Form extends React.Component {
   render() {
     return (
     <div class="container">
-        <form>
+        <form class="form">
+          
+            
         <div class="row">
-        <div class="form">
-          <label for="fname">
-              Full name
-          </label>
-         </div>
+        
+          <label for="fname"></label>
+       
          <div class="inputs" id="fname">
             <input type="text" name="firstName" placeholder="First name" onChange={this.handleChange} />
             <input type="text" name="secondName" placeholder="Last name" onChange={this.handleChange} />
@@ -42,66 +42,66 @@ class Form extends React.Component {
           </div>
           
           <div class="row">
-            <div class="form">
-              <label for="email">Email</label>
-           </div>
-            <div class="inputs">
+            
+              <label for="email"></label>
+         
+            
               <input type="text" id="email" name="email" placeholder="Your email" onChange={this.handleChange} />
-           </div>
+           
           </div>
           
           <div class="row">
-            <div class="form">
-              <label for="address">Address</label>
-            </div>
-            <div class="inputs" id="address">
-            <input type="text" name="street1" placeholder=" Address line 1" onChange={this.handleChange} />
-            <input type="text" name="street2" placeholder="Addrees line 2" onChange={this.handleChange} />
-            <input type="text" name="city" placeholder="City" onChange={this.handleChange} />
-            <input type="text" name="state" placeholder="State" onChange={this.handleChange} />
-            <input type="text" name="zip" placeholder="Zip" onChange={this.handleChange} />
-            </div>
-          </div>
-          
-          <div class="row">
-            <div class="form">
-              <label for="phone">Phone</label>
-            </div>
-            <div class="inputs">
-              <input type="text" id="phone" name="phone" placeholder="Phone #" onChange={this.handleChange} />
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="form">
-              <label for="payment">Payment Info</label>
-            </div>
-            <div class="inputs" id="payment">
-              <input type="text" name="ccNum" placeholder="Card #" onChange={this.handleChange} />
-            <input type="text" name="exp" placeholder="Card exp date" onChange={this.handleChange} />
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="form">
-              <label for="quantity">Quantity</label>
-            </div>
-            <div class="inputs">
-              <input type="text" id="quantity" name="quantity" placeholder="0"onChange={this.handleChange} />
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="form">
-              <label for="amount">Amount</label>
-            </div>
-            <div class="inputs">
-              <input type="text" id="amount" name="amount" placeholder="0.0" onChange={this.handleChange} />
+            
+              <label for="address"></label>
+            
+            <div  id="address">
+            <input type="text" class="ad1" name="street1" placeholder=" Address line 1" onChange={this.handleChange} />
+            <input type="text" class="ad2" name="street2" placeholder="Addrees line 2" onChange={this.handleChange} />
+            <input type="text"  name="city" placeholder="City" onChange={this.handleChange} />
+            <input type="text"  name="state" placeholder="State" onChange={this.handleChange} />
+            <input type="text"  name="zip" placeholder="Zip" onChange={this.handleChange} />
             </div>
           </div>
           
+          <div class="row">
+            
+              <label for="phone"></label>
+            
+            
+              <input type="tel" id="phone" name="phone" placeholder="Phone #" onChange={this.handleChange} />
+            
+          </div>
 
-          <input type="submit" value="Submit" />
+          <div class="row">
+            
+              <label for="payment"></label>
+            
+            <div id="payment">
+              <input type="text" class="inputs" name="ccNum" placeholder="Card #" onChange={this.handleChange} />
+            <input type="date" class="inputs" name="exp" placeholder="Card exp date" onChange={this.handleChange} />
+            </div>
+          </div>
+
+          <div class="row">
+            
+              <label for="quantity"></label>
+            
+            
+              <input type="text" class="inputs" id="quantity" name="quantity" placeholder="0"onChange={this.handleChange} />
+           
+          </div>
+
+          <div class="row">
+            
+              <label for="amount"></label>
+            
+            
+              <input type="text" class="inputs" id="amount" name="amount" placeholder="0.0" onChange={this.handleChange}/>
+            
+          </div>
+          
+          <div class="row"><button type="submit">Submit</button></div>
+          
           
         </form>
      </div>
